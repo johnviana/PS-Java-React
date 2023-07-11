84338db9674f5f917c5f6baa9b2b6603d03f151e
wedsysa <johnvianaluz@hotmail.com>
2023-07-11 16:13:37 -0300

    criando na class conta os campos

--- src/main/java/br/com/banco/domain/model/Conta.java
+++ src/main/java/br/com/banco/domain/model/Conta.java
@@ -1,4 +1,20 @@
 package br.com.banco.domain.model;
 
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "conta")
 public class Conta {
+
+    @Id
+    @Column(nullable = false)
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_conta;
+
+    @Column(nullable = false)
+    private String nome_responsavel;
 }
