4fe2d619945018cc0709c20410b2283ba7f64e9e
wedsysa <johnvianaluz@hotmail.com>
2023-07-11 16:27:03 -0300

    criando na class conta o get e set com lombok

--- src/main/java/br/com/banco/domain/model/Conta.java
+++ src/main/java/br/com/banco/domain/model/Conta.java
@@ -1,9 +1,11 @@
 package br.com.banco.domain.model;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+@Data
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "conta")
