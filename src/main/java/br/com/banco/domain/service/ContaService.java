e82528724d0c89eaa8516d8bfdae09432f9ee1b0
wedsysa <johnvianaluz@hotmail.com>
2023-07-11 16:55:58 -0300

    criando o service de ContaService metodo ListrConta

---
+++ src/main/java/br/com/banco/domain/service/ContaService.java
@@ -0,0 +1,19 @@
+package br.com.banco.domain.service;
+
+import br.com.banco.domain.model.Conta;
+import br.com.banco.domain.repository.ContaRepopsitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContaService {
+
+    @Autowired
+    private ContaRepopsitory contaRepopsitory;
+
+    public List<Conta> listarContra(){
+        return  contaRepopsitory.findAll();
+    }
+}
