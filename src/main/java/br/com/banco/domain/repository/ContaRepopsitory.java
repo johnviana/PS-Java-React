d74a3f5a57f99fc2b77c593070c2587e603e0419
wedsysa <johnvianaluz@hotmail.com>
2023-07-11 22:58:31 -0300

    Adicionando component repository na classe

--- src/main/java/br/com/banco/domain/repository/ContaRepopsitory.java
+++ src/main/java/br/com/banco/domain/repository/ContaRepopsitory.java
@@ -2,6 +2,9 @@ package br.com.banco.domain.repository;
 
 import br.com.banco.domain.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContaRepopsitory extends JpaRepository<Conta, Long> {
+
 }
