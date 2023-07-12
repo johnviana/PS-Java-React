ba7f494f08a6bbf2e94575554f0ca3b63e0937b1
wedsysa <johnvianaluz@hotmail.com>
2023-07-11 22:04:15 -0300

    metodo buscarOuFalhar criado na classe ContaService para buscar uma conta caso tenha ou nao

--- src/main/java/br/com/banco/domain/service/ContaService.java
+++ src/main/java/br/com/banco/domain/service/ContaService.java
@@ -1,5 +1,6 @@
 package br.com.banco.domain.service;
 
+import br.com.banco.domain.exception.ContaNaoEncontradaException;
 import br.com.banco.domain.model.Conta;
 import br.com.banco.domain.repository.ContaRepopsitory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,13 @@ public class ContaService {
 
     public Conta salvar(Conta conta){
         return contaRepopsitory.save(conta);
+    }
+
+    public Conta buscarOuFalhar(Long id){
+        return contaRepopsitory.findById(id)
+                .orElseThrow(() -> new ContaNaoEncontradaException(
+                        String.format("Codigo da conta não encontrado %d", id)));
+
     }
 
 
