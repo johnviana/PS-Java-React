0910c93ef6730d06fa53974e3880ef635f70fe8a
wedsysa <johnvianaluz@hotmail.com>
2023-07-14 10:26:34 -0300

    feat: metodo de listar tranferencia com conta cirada no Repository Transferencia

--- src/main/java/br/com/banco/domain/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/domain/repository/TransferenciaRepository.java
@@ -4,9 +4,11 @@ import br.com.banco.domain.model.Transferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+public interface TransferenciaRepository extends JpaRepository<Transferencia, Long>, TransferenciaRepositoryQueries {
 
-
+    List<Transferencia> findByContaId(Long contaId);
 
 }
