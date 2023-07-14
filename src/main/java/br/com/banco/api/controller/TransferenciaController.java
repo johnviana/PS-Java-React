0b13fb8eeb4d64ddc3fd64345f884240e24eab9a
wedsysa <johnvianaluz@hotmail.com>
2023-07-14 10:50:47 -0300

    feat: metodo getTransferencias criado nem TransferenciaController.

--- src/main/java/br/com/banco/api/controller/TransferenciaController.java
+++ src/main/java/br/com/banco/api/controller/TransferenciaController.java
@@ -1,15 +1,17 @@
 package br.com.banco.api.controller;
 
 import br.com.banco.domain.model.Transferencia;
-import br.com.banco.domain.service.ContaService;
+import br.com.banco.domain.repository.TransferenciaRepository;
 import br.com.banco.domain.service.TransferenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -20,21 +22,32 @@ public class TransferenciaController {
     private TransferenciaService transferenciaService;
 
     @Autowired
-    private ContaService contaService;
+    private TransferenciaRepository transferenciaRepository;
+
 
     @GetMapping
-    public ResponseEntity<List<Transferencia>> getTranfesrencia(){
-        List<Transferencia> transferencias = transferenciaService.listaTransferencia();
+    public ResponseEntity<List<Transferencia>> getTransferencias(
+            @RequestParam(required = false) Long contaId,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate,
+            @RequestParam(required = false) String nomeOperadorTransacao
+    ) {
+        List<Transferencia> transferencias;
+        if (contaId != null) {
+            transferencias = transferenciaService.obterTransferenciasPorConta(contaId);
+        } else if (startDate != null && endDate != null && nomeOperadorTransacao != null) {
+            transferencias = transferenciaService.obterTransferenciasPorPeriodoENomeOperador(
+                    startDate, endDate, nomeOperadorTransacao
+            );
+        } else if (startDate != null && endDate != null) {
+            transferencias = transferenciaService.obterTransferenciasPorPeriodo(startDate, endDate);
+        } else if (nomeOperadorTransacao != null) {
+            transferencias = transferenciaService.obterTransferenciasPorNomeOperador(nomeOperadorTransacao);
+        } else {
+            transferencias = transferenciaService.obterTodasTransferencias();
+        }
+
         return ResponseEntity.ok(transferencias);
     }
 
-    @GetMapping("/{id}")
-    public Transferencia transferenciaId(@PathVariable Long id){
-        Transferencia transferencias = transferenciaService.buscarOuFalhar(id);
-        return transferencias;
-        }
-
-    }
-
-
-
+}
