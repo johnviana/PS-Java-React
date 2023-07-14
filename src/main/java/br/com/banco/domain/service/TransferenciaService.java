353a6d3747a0cfc718dfbe7a87ad9b12dc09d211
wedsysa <johnvianaluz@hotmail.com>
2023-07-14 10:29:07 -0300

    feat: metodo obter Transferencias Por Conta criada no Service transferencia

--- src/main/java/br/com/banco/domain/service/TransferenciaService.java
+++ src/main/java/br/com/banco/domain/service/TransferenciaService.java
@@ -5,7 +5,6 @@ import br.com.banco.domain.repository.TransferenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 /**
