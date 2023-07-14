4684bec6cb476a8a413979be90c6b5fbd752ac7c
wedsysa <johnvianaluz@hotmail.com>
2023-07-14 10:33:01 -0300

    feat: metodo para retornar todas as transferências relacionadas a aquele periodo de tempo. no repository transferencia

--- src/main/java/br/com/banco/domain/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/domain/repository/TransferenciaRepository.java
@@ -4,11 +4,15 @@ import br.com.banco.domain.model.Transferencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long>, TransferenciaRepositoryQueries {
 
     List<Transferencia> findByContaId(Long contaId);
+
+    List<Transferencia> findByDataTransferenciaBetween(LocalDateTime startDate, LocalDateTime endDate);
+
 
 }
