2d85ebe1a7086ac61750b050b4c7c49d9f43ee0e
wedsysa <johnvianaluz@hotmail.com>
2023-07-14 10:28:51 -0300

    feat: metodo obter Transferencias Por Conta criada no Service transferencia

--- src/main/java/br/com/banco/domain/service/TransferenciaService.java
+++ src/main/java/br/com/banco/domain/service/TransferenciaService.java
@@ -1,27 +1,27 @@
 package br.com.banco.domain.service;
 
-import br.com.banco.domain.exception.EntidadeNaoEncontradaException;
 import br.com.banco.domain.model.Transferencia;
 import br.com.banco.domain.repository.TransferenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+/**
+ * @author johnv
+ */
 
 @Service
 public class TransferenciaService {
 
     @Autowired
-    private TransferenciaRepository transferenciaRepository;
+    private  TransferenciaRepository transferenciaRepository;
 
 
-    public List<Transferencia> listaTransferencia(){
-        return transferenciaRepository.findAll();
+    public List<Transferencia> obterTransferenciasPorConta(Long contaId) {
+        return transferenciaRepository.findByContaId(contaId);
     }
 
-    public Transferencia buscarOuFalhar(Long id){
-        return transferenciaRepository.findById(id)
-                .orElseThrow(() -> new EntidadeNaoEncontradaException(
-                        String.format("Codigo de Transferencia não encontrado ", id)));
-    }
+
 }
